d22ffeb05463d55dd20e31ca4ffccd56c4a9776e
irisfaraway <iris.faraway@gmail.com>
2017-11-08 11:41:28 +0000

    Fix linter issues

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "Books", type: :request do
-  describe "GET /books" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'Books', type: :request do
+  describe 'GET /books' do
+    it 'returns a successful response' do
       get books_path
       expect(response).to have_http_status(200)
     end
